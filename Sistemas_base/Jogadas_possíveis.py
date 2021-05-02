1f400bbe37fec35aa2eac38efa903b99c2d70038
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2021-05-02 20:51:04 -0300

    Update Jogadas_possíveis.py

--- Sistemas_base/Jogadas_possíveis.py
+++ Sistemas_base/Jogadas_possíveis.py
@@ -7,7 +7,6 @@ def extrai_naipe(carta):
         if t == '♦' or t =='♥' or t == '♣' or t == '♠':
 
             return t
-
 def extrai_valor(carta):
 
     carta = str(carta)
@@ -19,48 +18,26 @@ def extrai_valor(carta):
     else:
 
         return carta[0]
-
-
-def lista_movimentos_possiveis(baralho,indice):
-
-    
-    jogadas = []
-    
-    
+def lista_movimentos_possiveis(baralho,indice):    
+    jogadas = []    
     if indice == 0:
-
-        return jogadas
-    
-    
+        return jogadas       
     naipe = extrai_naipe(baralho[indice])
-
     naipe_anterior = extrai_naipe(baralho[indice - 1])
-
-    if indice >= 3:
-        
+    if indice >= 3:        
         naipe_terceiro = extrai_naipe(baralho[indice - 3])
-
     valor = extrai_valor(baralho[indice])
-
-    valor_anterior = extrai_valor(baralho[indice - 1])
-    
+    valor_anterior = extrai_valor(baralho[indice - 1])    
     if indice >= 3:
-
         valor_terceiro = extrai_valor(baralho[indice - 3])
-
     if naipe == naipe_anterior or valor == valor_anterior:
-
         jogadas.append(1)
-
     if indice >= 3:
-
         if naipe == naipe_terceiro or valor == valor_terceiro:
-
-            jogadas.append(3)
-    print(naipe_anterior)
+            jogadas.append(3)    
     return jogadas
 
-print(lista_movimentos_possiveis(['6♥', 'J♥', '9♣', '9♥'],1))
+#print(lista_movimentos_possiveis(['6♥', 'J♥', '9♣', '9♥'],1))
 
 
         
