e8ded7273bb473c4b255c4c36a110bf3a2ffa66b
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2021-05-02 21:14:39 -0300

    Create Solitaire-Accordion.py

---
+++ Solitaire-Accordion.py
@@ -0,0 +1,7 @@
+from Sistemas_base.Cria_baralho import *
+from Sistemas_base.Empilha_carta import *
+from Sistemas_base.Extrai_naipe import *
+from Sistemas_base.Extrai_valor import *
+from Sistemas_base.Jogadas_possíveis import *
+
+# CÓDIGO DO JOGO AQUI 
