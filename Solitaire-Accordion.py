c50248aa2c748a48a287b6bdf5fdf0e73f7a74df
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2021-05-02 23:03:28 -0300

    Correção de bugs, escolha da segunda carta e empilhamento

--- Solitaire-Accordion.py
+++ Solitaire-Accordion.py
@@ -7,12 +7,19 @@ from Sistemas_base.Jogadas_possíveis import *
 from Sistemas_base.possui_movimentos import *
 
 print("Bem vindo ao Paciência Acordeão! 🇧🇷\nWelcome to Solitaire Accordion! 🇺🇸\n==================================")
+print("\nIniciando primeiro jogo... (starting first game...)\n")
+
+def ImprimeEstadoAtual(baralho):
+    print("Estado atual do baralho: (Current state of the deck: )")
+    for i in range(len(baralho)):
+        print(str(i+1) + ": " + baralho[i])
 
 def Jogar():
     pilha = []
     baralho = cria_baralho()
     random.shuffle(baralho)
     pilha = baralho
+    ImprimeEstadoAtual(pilha)
     while True:
         if len(pilha) == 1:
             print("VOCÊ VENCEU O JOGO! (YOU WON!)")
@@ -20,14 +27,21 @@ def Jogar():
         if not possui_movimentos_possiveis(pilha):
             print("VOCÊ PERDEU O JOGO! (YOU LOST!)")
             break
-        print("Estado atual do baralho: (Current state of the deck: )")
-        for i in range(len(baralho)):
-            print(str(i+1) + ": " + baralho[i])
-        alvo = input("Escolha uma carta para trocar (Choose a card) [2-{}]".format(len(pilha)))
+        alvo = input("Escolha uma carta para mover (Choose a card) [2-{}]".format(len(pilha)))
         if not alvo.isdigit() or (alvo.isdigit() and int(alvo) <= 1) or (alvo.isdigit() and int(alvo) > len(pilha)):
             print("Input inválido! Escolha um número entre 2 e {} (Invalid Input)".format(len(pilha)))
+        elif lista_movimentos_possiveis(pilha, int(alvo)-1) == []:
+            print("Não é possível mover essa carta! Escolha outra carta (You must choose a different card)")
         else:
-            pass
+            while True:
+                alvo2 = input("Escolha a carta sobre a qual você quer empilhar a primeira (Choose second card)")
+                if int(alvo2) not in lista_movimentos_possiveis(pilha, int(alvo)-1):
+                    print("Segunda carta inválida! Escolha outro movimento (Invalid second card)")
+                    print(lista_movimentos_possiveis(pilha, int(alvo)-1))
+                else:
+                    empilha(pilha, int(alvo)-1, int(alvo2)-1)
+                    ImprimeEstadoAtual(pilha)
+                    break
 
 def Denovo():
     denovo = input("\nJogar de novo?(Play again?) (Y/N)")
