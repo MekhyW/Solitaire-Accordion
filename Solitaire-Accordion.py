5842d40927c14f9b54ccc3d32c7a33bc56409329
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2021-05-02 21:44:20 -0300

    Embaralha e obtém uma pilha com 4 cartas

--- Solitaire-Accordion.py
+++ Solitaire-Accordion.py
@@ -1,14 +1,20 @@
+import random
 from Sistemas_base.Cria_baralho import *
 from Sistemas_base.Empilha_carta import *
 from Sistemas_base.Extrai_naipe import *
 from Sistemas_base.Extrai_valor import *
 from Sistemas_base.Jogadas_possíveis import *
 
+print("Bem vindo ao Paciência Acordeão! 🇧🇷\nWelcome to Solitaire Accordion! 🇺🇸\n==================================")
+
 def Jogar():
-    pass
+    baralho = cria_baralho()
+    random.shuffle(baralho)
+    pilha = [baralho[0], baralho[1], baralho[2], baralho[3]]
+    print(pilha)
 
 def Denovo():
-    denovo = input("Jogar novamente? (S/N)")
+    denovo = input("\nJogar de novo?(Play again?) (Y/N)")
     if denovo == "N" or denovo == "n":
         exit()
 
