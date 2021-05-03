47b75c90c3e0efe9bb2c8267b29913f020cc17cf
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2021-05-02 21:54:42 -0300

    Checa condições de vitória/derrota

--- Sistemas_base/possui_movimentos.py
+++ Sistemas_base/possui_movimentos.py
@@ -1,6 +1,35 @@
-from Extrai_naipe import *
-from Extrai_valor import *
-from Jogadas_possíveis import *  
+def extrai_naipe(carta):
+    carta = str(carta)
+    for t in carta:
+        if t == '♦' or t =='♥' or t == '♣' or t == '♠':
+            return t
+
+def extrai_valor(carta):
+    carta = str(carta)
+    if '10' in carta:
+        return '10'
+    else:
+        return carta[0]
+
+def lista_movimentos_possiveis(baralho,indice):    
+    jogadas = []    
+    if indice == 0:
+        return jogadas       
+    naipe = extrai_naipe(baralho[indice])
+    naipe_anterior = extrai_naipe(baralho[indice - 1])
+    if indice >= 3:        
+        naipe_terceiro = extrai_naipe(baralho[indice - 3])
+    valor = extrai_valor(baralho[indice])
+    valor_anterior = extrai_valor(baralho[indice - 1])    
+    if indice >= 3:
+        valor_terceiro = extrai_valor(baralho[indice - 3])
+    if naipe == naipe_anterior or valor == valor_anterior:
+        jogadas.append(1)
+    if indice >= 3:
+        if naipe == naipe_terceiro or valor == valor_terceiro:
+            jogadas.append(3)    
+    return jogadas
+
 #----------A parte de cima ja faz parte dos outos códigos-----------
 def possui_movimentos_possiveis(baralho):
     t = 0
--- Solitaire-Accordion.py
+++ Solitaire-Accordion.py
@@ -4,14 +4,23 @@ from Sistemas_base.Empilha_carta import *
 from Sistemas_base.Extrai_naipe import *
 from Sistemas_base.Extrai_valor import *
 from Sistemas_base.Jogadas_possíveis import *
+from Sistemas_base.possui_movimentos import *
 
 print("Bem vindo ao Paciência Acordeão! 🇧🇷\nWelcome to Solitaire Accordion! 🇺🇸\n==================================")
 
 def Jogar():
-    baralho = cria_baralho()
-    random.shuffle(baralho)
-    pilha = [baralho[0], baralho[1], baralho[2], baralho[3]]
-    print(pilha)
+    pilha = []
+    while not possui_movimentos_possiveis(pilha):
+        baralho = cria_baralho()
+        random.shuffle(baralho)
+        pilha = [baralho[0], baralho[1], baralho[2], baralho[3]]
+    while True:
+        if len(pilha) == 1:
+            print("VOCÊ VENCEU O JOGO! (YOU WON!)")
+            break
+        if not possui_movimentos_possiveis(pilha):
+            print("VOCÊ PERDEU O JOGO! (YOU LOST!)")
+            break
 
 def Denovo():
     denovo = input("\nJogar de novo?(Play again?) (Y/N)")
